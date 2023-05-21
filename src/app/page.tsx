2265d432df0d308b6b26d062173b0d673d14df45
이영우 <zestlee1106@gmail.com>
2023-05-21 16:33:52 +0900

    푸시 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -53,7 +53,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+            뭔가 테스트로 실행해보자
           </p>
         </a>
 
