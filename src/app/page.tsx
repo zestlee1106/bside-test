620ae4443530a59b3584490fd196b4896eb58b47
이영우 <zestlee1106@gmail.com>
2023-05-21 16:58:38 +0900

    push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -53,7 +53,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            뭔가 테스트로 실행해보자231232394023
+            뭔가 테스트로 x
           </p>
         </a>
 
